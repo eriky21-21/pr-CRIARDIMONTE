a0d0a23bbbf3e56d8fa202a23fb9484a931990db
eriky21-21 <eriky21@gmail.com>
2025-08-21 00:23:02 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,3 +5,14 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  preview: {
+    host: "0.0.0.0",
+    port: 3000,
+    allowedHosts: ["poetic-liberation-production.up.railway.app"],
+  },
+});
