de69eec4a89ade755389101457308b5d81bdb21c
eriky21-21 <eriky21@gmail.com>
2025-08-21 15:49:21 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,12 @@
 // src/supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-// ⚠️ Railway precisa ter essas variáveis no painel
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// 🔑 pegue as chaves no dashboard do Supabase
+const https://muxzmgxblgwhlnkqgyei.supabase.co
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+ = import.meta.env.VITE_SUPABASE_URL;
+const eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im11eHptZ3hibGd3aGxua3FneWVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUzMTE3MjIsImV4cCI6MjA3MDg4NzcyMn0.N52hXa62IN6VV0QW8nP6f8A_LPHFSXHxSXvf4pfpvtg
+ = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// Cria a instância do cliente Supabase
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
