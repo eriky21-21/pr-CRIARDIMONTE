ae7318c70667619d07d78d226ae08f6efbc8e3e7
eriky21-21 <eriky21@gmail.com>
2025-08-21 16:06:20 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,9 +1,9 @@
 // src/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-// 🔑 Pega as variáveis do arquivo .env
+// URL e Key vêm do arquivo .env
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Cria a instância do cliente Supabase
+// Cria a instância do Supabase
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
