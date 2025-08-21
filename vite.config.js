67fd875db22579e3153a41b8a952248001de4c9f
eriky21-21 <eriky21@gmail.com>
2025-08-21 00:26:32 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+// Configuração do Vite
 export default defineConfig({
   plugins: [react()],
-});
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173, // porta padrão do Vite
+  },
   preview: {
     host: "0.0.0.0",
     port: 3000,
