d13d83ec438da24d2a8af99f9f9e1ef000de16e6
eriky21-21 <eriky21@gmail.com>
2025-08-21 01:44:55 -0300

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,8 +0,0 @@
-// supabaseClient.js
-import { createClient } from '@supabase/supabase-js'
-
-// Variáveis de ambiente (confira no Railway)
-const supabaseUrl = process.env.SUPABASE_URL
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
