15ce3a9fd1b916657bb9dcfe443c2699a188b77f
eriky21-21 <eriky21@gmail.com>
2025-08-23 01:24:36 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // Railway define a porta via process.env.PORT
 export default defineConfig({
+  base: "./", // 👈 garante que os assets sejam encontrados no deploy
   plugins: [react()],
   server: {
     port: process.env.PORT || 3000,
